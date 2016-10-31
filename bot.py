5bf4c17f58cf8e8b312f6cea4eb78c4af281e9be
ethernetcable <ethernetcable@users.noreply.github.com>
2016-10-31 13:32:53 +0000

    did the thing
    <insert optional extended description here>

--- bot.py
+++ bot.py
@@ -16,15 +16,17 @@ twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 # tweets every other word of someone's tweets for some reason
 def every_other(acc):
     try:
+        # get id of latest tweet
         orig_tl = twitter.get_user_timeline(screen_name=acc, count=1)
         orig_twt = orig_tl[0]
         orig_id = orig_twt['id_str']
         id = orig_id
         while True:
+            # gets id of latest tweet on timeline every 20 secs
             g_timeline = twitter.get_user_timeline(screen_name=acc, count=1)
             g_tweet = g_timeline[0]
             g_id = g_tweet['id_str']
-            if g_id != id: # in case of a new tweet
+            if g_id != id: # in case of new tweet
                 g_tweet = g_tweet['text'].split()
                 g_tweet = ' '.join(g_tweet[::2])
                 # remove links
